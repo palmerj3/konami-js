6bb70e9ea731376d0fcdb6f946a246fd46f39453
Jason Palmer <jpalmer@spotify.com>
2012-06-09 09:22:48 -0400

    Initial push

---
+++ konami.js
@@ -0,0 +1,37 @@
+var Konami = function () {
+  'use strict';
+
+  var CODE = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13],
+    progress = [],
+    callback = null,
+    check_progress = function () {
+      var code = CODE.slice(0),
+        segment = code.splice(0, progress.length);
+
+      if (progress.join(',') === segment.join(',')) {
+        if (progress.length === CODE.length) {
+          progress = [];
+          callback();
+        }
+      } else {
+        progress = [];
+      }
+    },
+    keydown_listener = function (e) {
+      progress.push(e.keyCode);
+      check_progress();
+    },
+    initialize_listeners = function () {
+      window.addEventListener('keydown', keydown_listener, false);
+    };
+
+  initialize_listeners();
+
+  return {
+    onSuccess: function (c) {
+      if (typeof (c) === 'function') {
+        callback = c;
+      }
+    }
+  };
+};
