569e4e3ac6d7568fa87f1b81f317301540947cf0
Jason Palmer <jpalmer@spotify.com>
2012-06-10 11:45:20 -0400

    Refactored the code a bit so it is more flexible.  Placed success callback inside the closure so it can be set directly.  Also added the ability to specify a listener target other than window, so someone could use this on an input field or something like that if they wanted

--- README.md
+++ README.md
@@ -3,13 +3,26 @@ konami-js
 
 Simple system for detecting the entry of the famous Konami code and attaching an event
 
-Example:
+Simple Example:
 
 ```
 (function() {
   var konami = new Konami();
-  konami.onSuccess(function() {
+  konami.onSuccess = function () {
     alert('You win!');
-  });
+  };
+  konami.init();
+}());
+```
+
+Example with specific target:
+```
+(function() {
+  var konami = new Konami();
+  konami.onSuccess = function () {
+    alert('You win!');
+  };
+  konami.listenerTarget = document.getElementById('my_element');
+  konami.init();
 }());
 ```
--- konami.js
+++ konami.js
@@ -1,43 +1,44 @@
 var Konami = function () {
   'use strict';
+  var _CODE = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13],
+    _listenerTarget = null,
+    _onSuccess = null,
+    _progress = [],
+    _check_progress = function () {
+      var code = _CODE.slice(0),
+        segment = code.splice(0, _progress.length);
 
-  var CODE = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13],
-    progress = [],
-    callback = null,
-    check_progress = function () {
-      var code = CODE.slice(0),
-        segment = code.splice(0, progress.length);
-
-      if (progress.join(',') === segment.join(',')) {
-        if (progress.length === CODE.length) {
-          progress = [];
-          callback();
+      if (_progress.join(',') === segment.join(',')) {
+        if (_progress.length === _CODE.length) {
+          _progress = [];
+          _onSuccess();
         }
       } else {
-        progress = [];
+        _progress = [];
       }
     },
-    keydown_listener = function (e) {
-      progress.push(e.keyCode);
-      check_progress();
+    _keydown_listener = function (e) {
+      _progress.push(e.keyCode);
+      _check_progress();
     },
-    addEventListeners = function () {
-      if (window.addEventListener) {
-          window.addEventListener('keydown', keydown_listener, false);
-      } else if (window.attachEvent) {
-          window.attachEvent('onkeydown', keydown_listener);
+    _addEventListeners = function () {
+      if (_listenerTarget.addEventListener) {
+          _listenerTarget.addEventListener('keydown', _keydown_listener, false);
+      } else if (_listenerTarget.attachEvent) {
+          _listenerTarget.attachEvent('onkeydown', _keydown_listener);
       } else {
-          window.onkeydown = keydown_listener;
+          _listenerTarget.onkeydown = _keydown_listener;
       }
     };
-
-  addEventListeners();
-
+  
   return {
-    onSuccess: function (c) {
-      if (typeof (c) === 'function') {
-        callback = c;
-      }
+    onSuccess : function (){},
+    listenerTarget : window,
+    init : function () {
+      _onSuccess = this.onSuccess;
+      _listenerTarget = this.listenerTarget;
+
+      _addEventListeners();
     }
-  };
+  }
 };
